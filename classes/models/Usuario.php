f4fa1c628221582fe413987955471c7fe8d932dc
Ana Lívia <liviaana46@gmail.com>
2018-04-25 14:06:05 -0300

    Novas alterações

--- classes/models/Usuario.php
+++ classes/models/Usuario.php
@@ -6,16 +6,7 @@ class Usuario extends Crud{
     private $nome, $email, $senha, $telefone, $tipo;
 
 
-    #construtor para atribuir valores aos atributos da classe
-   /* public function __construct($nome, $email, $senha, $telefone, $tipo){
-        $this->nome = $nome;
-        $this->email = $email;
-        $this->senha = $senha;
-        $this->telefone = $telefone;
-        $this->tipo = $tipo;
-    }*/
-
-    #este método é abstrato na classe pai, logo é forçada a implementação do mesmo na classe filho
+   
     public function insert($nome, $email, $senha, $telefone, $tipo){
         try{
             $sql = "INSERT INTO $this->table (nome, email, senha, telefone, tipo) VALUES (:nome, :email, :senha, :telefone, :tipo)";
@@ -35,11 +26,11 @@ class Usuario extends Crud{
     public function update($id){
         $sql = "UPDATE $this->table SET nome = :nome, email = :email, senha = :senha, telefone = :telefone, tipo = :tipo WHERE id = :id ";
         $prepare = DB::prepare($sql);
-        $prepare->bindValue(':nome', $this->nome, PDO::PARAM_STR);
-        $prepare->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $prepare->bindValue(':senha', $this->senha, PDO::PARAM_STR);
-        $prepare->bindValue(':telefone', $this->telefone, PDO::PARAM_STR);
-        $prepare->bindValue(':tipo', $this->tipo, PDO::PARAM_INT);
+        $prepare->bindValue(':nome', $nome, PDO::PARAM_STR);
+        $prepare->bindValue(':email', $email, PDO::PARAM_STR);
+        $prepare->bindValue(':senha', $senha, PDO::PARAM_STR);
+        $prepare->bindValue(':telefone', $telefone, PDO::PARAM_STR);
+        $prepare->bindValue(':tipo', $tipo, PDO::PARAM_INT);
         $prepare->bindValue(':id', $id, PDO::PARAM_INT);
         return $prepare->execute();
     }
--- itens/1cadastroi.php
+++ itens/1cadastroi.php
@@ -1,7 +1,7 @@
 
 <?php 
-	include("../classes/DB.php");
-	$conn= new DB();
+	include("../bd/Conexao.class.php");
+	$conn= new Conexao();
 	
 	//$connect = mysqli_connect("localhost","root","","achados") or die("ERROR");
 
@@ -16,18 +16,31 @@
 		
 		$status=0;
 		$id=($_COOKIE["id"]);
+		$uploaddir = '/var/www/uploads/';
+		$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+			
+			if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+   			 echo "Arquivo válido e enviado com sucesso.\n";
+			} else {
+    			echo "Possível ataque de upload de arquivo!\n";
+			}
+			echo 'Aqui está mais informações de debug:';
+			print_r($_FILES);
+
+			
+
 		
-
-
-		$sql="INSERT INTO intens(nome_item,nome_pessoa,local_encontrado,descricao,data_encontrado,status,imagem,id_usuarios) 
-			VALUES (:nome_item,:nome_pessoa,:localenc,:descricao,:data,:status,:imagem,:id)";
-
+		
+		
+		$sql="INSERT INTO intens(nome_item,nome_pessoa,local_encontrado,descricao,data_encontrado,status,id_usuarios) 
+			VALUES (:nome_item,:nome_pessoa,:local,:descricao,:data,:status,:id)";
 
 
 		$prepare = $conn->prepare($sql);
 		$prepare->bindValue(':nome_item',$nome_item, PDO::PARAM_STR);
 		$prepare->bindValue(':nome_pessoa',$nome_pessoa, PDO::PARAM_STR);
-		$prepare->bindValue(':localenc',$local, PDO::PARAM_STR);
+		$prepare->bindValue(':local',$local, PDO::PARAM_STR);
 		$prepare->bindValue(':descricao',$descricao, PDO::PARAM_STR);
 		$prepare->bindValue(':data',$data, PDO::PARAM_STR);
 		$prepare->bindValue(':status',$status, PDO::PARAM_STR);
@@ -38,6 +51,7 @@
 		
 		echo "Item registrado";
 		}else{
+
 		die("ERROR");
 
 		}
