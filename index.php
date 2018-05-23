16489bddfb583921be573a32cc49e37c8a54b54b
Ana Lívia <liviaana46@gmail.com>
2018-05-23 16:05:33 -0300

    Erros

--- index.php
+++ index.php
@@ -166,7 +166,7 @@
                 </div>
                 <div class="card-content">
                   <span class="card-title activator grey-text text-darken-4"><?php echo $row->nome_item;  ?><i class="material-icons right">more_vert</i></span>
-                    <input type="text" class="id-card" value="<?php echo $row->id ?>">
+                    <input type="hidden" class="id-card" value="<?php echo $row->id ?>">
                 </div>
                 <div class="card-action">
                         <?php echo '<a href="index.php?action=delete&id=' . $row->id.'">Excluir</a>' ?>
--- itens/cadastroi.php
+++ itens/cadastroi.php
@@ -29,7 +29,7 @@
 		
 
 //*****************ATUAILIZAR******************
-		if ($_POST["acao"] == "update") {
+		if (isset($_POST["update"])) {
 			$acao = 0;
 		$nome_item=($_POST["nome"]);
 		$nome_pessoa=($_POST["nome_pessoa"]);
